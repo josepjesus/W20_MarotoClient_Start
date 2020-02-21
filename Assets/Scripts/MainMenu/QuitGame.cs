4e3ed832e520b20874960742fb0d40f1df4790e4
josepjesus <pepjesus@hotmail.es>
2020-02-21 17:35:14 +0100

    initial commit

---
+++ Assets/Scripts/MainMenu/QuitGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+
+    public void OnQuitGameButtonClicked()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+         Application.OpenURL(webplayerQuitURL);
+#else
+         Application.Quit();
+#endif
+    }
+}
